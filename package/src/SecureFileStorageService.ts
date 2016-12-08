818a7cb6ad461d59cf4e156ad1e58c5e06a39e2a
Ronan Janvresse <ronan.janvresse@bpce.fr>
2016-12-08 11:51:40 +0100

    Change Default to false

--- package/src/SecureFileStorageService.ts
+++ package/src/SecureFileStorageService.ts
@@ -65,7 +65,7 @@ export class SecureFileStorageService {
         return deferred.promise;
     }
 
-    public write(key: string, data: any, iCloudSync:boolean = true): angular.IPromise<any> {
+    public write(key: string, data: any, iCloudSync:boolean = false): angular.IPromise<any> {
         var deferred: ng.IDeferred<any> = this.$q.defer();
         this.securityApiReady().then((isReady: boolean) => {
             if (isReady) {
